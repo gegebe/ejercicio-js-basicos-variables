43ba773170ec6845e6216e324ed6d31d16355265
gegebe <ggirbes@gmail.com>
2023-10-06 12:40:45 +0200

    finished

--- ex8.js
+++ ex8.js
@@ -3,31 +3,33 @@
 let texto = "singulars"; // Si cambiaramos esta palabra, debería seguir funcionando todos los ejercicios
 
 console.log("Ejercicio 1: Longitud del string"); 
-console.log();  // por ejemplo, aquí, entre los paréntesis, tenemos que poner: texto.length
+console.log(texto.length);  // por ejemplo, aquí, entre los paréntesis, tenemos que poner: texto.length
 
 console.log("Ejercicio 2: Primer carácter del string"); 
-console.log(); 
+console.log(texto.charAt(0)); 
 
 console.log("Ejercicio 3: Último carácter del string"); 
-console.log(); 
+console.log(texto.charAt(texto.length - 1)); 
 
 console.log("Ejercicio 4: Primeros 3 caracteres del string"); 
-console.log(); 
+//console.log(texto.substring(0, 3)); 
+console.log(texto.match(/[\w]{3}/));
+//console.log(texto.match(/^\w+/))
 
 console.log("Ejercicio 5: Últimos 3 caracteres del string"); 
-console.log(); 
+console.log(texto.substring(6,9)); 
 
 console.log("Ejercicio 6: String en mayúsculas"); 
-console.log(); 
+console.log(texto.toUpperCase()); 
 
 console.log("Ejercicio 7: String en minúsculas"); 
-console.log(); 
+console.log(texto.toLowerCase()); 
 
 console.log("Ejercicio 8: Devuelve el índice del primer carácter 's'"); 
-console.log(); 
+console.log(texto.indexOf("s")); 
 
 console.log("Ejercicio 9: Posición del último carácter 's'"); 
-console.log(); 
+console.log(texto.lastIndexOf("s"));
 
 console.log("Ejercicio 10: String en orden inverso"); 
-console.log(); 
+console.log(texto.split("").reverse().join("")); 
